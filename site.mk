f98e72e91e8f7cfff90c96b7dde268f01898748f
Kris <kristian@stoeckel-mail.de>
2016-07-05 13:30:25 +0200

    Update site.mk

--- site.mk
+++ site.mk
@@ -37,7 +37,7 @@ GLUON_SITE_PACKAGES := \
 #			opkg compare-versions "$1" '>>' "$2"
 #		to decide if a version is newer or not.
 
-DEFAULT_GLUON_RELEASE := 0.2.1
+DEFAULT_GLUON_RELEASE := 0.2.2-exp
 
 
 ##	GLUON_RELEASE
